c6c48c56ac149dc21bb173b505710c68debecdad
engsyst <eng.syst@gmail.com>
2017-03-04 15:31:14 +0200

    Resolved conflict

--- LB3WSServerJAXWSExampleOnTomcat/src/ua/nure/itech/jaxws/service/Hello.java
+++ LB3WSServerJAXWSExampleOnTomcat/src/ua/nure/itech/jaxws/service/Hello.java
@@ -1,17 +1,10 @@
 package ua.nure.itech.jaxws.service;
 
 import javax.jws.WebService;
-<<<<<<< HEAD
 //import javax.ws.rs.GET;
 //import javax.ws.rs.Path;
 //import javax.ws.rs.Produces;
 //import javax.ws.rs.core.MediaType;
-=======
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
->>>>>>> branch 'master' of https://github.com/engsyst/ws.git
 
 /**
  * Web-service class ua.nure.itech.jaxws.service.Hello
@@ -19,18 +12,10 @@ import javax.ws.rs.core.MediaType;
  * @author engsyst
  */
 
-<<<<<<< HEAD
 //@Path("hello/{userName")
-=======
-@Path("hello/{userName")
->>>>>>> branch 'master' of https://github.com/engsyst/ws.git
 @WebService
 public class Hello {
-<<<<<<< HEAD
 //	@GET
-=======
-	@GET
->>>>>>> branch 'master' of https://github.com/engsyst/ws.git
 	public String hello(String name) {
 		return "Hello " + name + "!";
 	}
