743bbfd810850cb925fa3f62e96c07dea1784d96
engsyst <eng.syst@gmail.com>
2017-04-04 18:33:04 +0300

    Querys: removed db perfix from queries

--- wsWebClient/src/ua/nure/order/server/dao/mysql/Querys.java
+++ wsWebClient/src/ua/nure/order/server/dao/mysql/Querys.java
@@ -1,7 +1,7 @@
 package ua.nure.order.server.dao.mysql;
 
 public interface Querys {
-	static final String SQL_ADD_BOOK = "INSERT INTO `ws`.`book` "
+	static final String SQL_ADD_BOOK = "INSERT INTO `book` "
 			+ "(`title`, `isbn`, `price`, `count`, `category_id`) "
 			+ "VALUES (?, ?, ?, ?, ?);";
 	
@@ -25,7 +25,7 @@ public interface Querys {
 	 *  use makeAuthorsValues(List? authors, int bookId)
 	 *  to generate values string in format: (name1,n),(name2,n) ... ;
 	 */
-	static final String SQL_ADD_AUTHOR = "INSERT INTO `ws`.`author` "
+	static final String SQL_ADD_AUTHOR = "INSERT INTO `author` "
 			+ "(`title`) VALUES ";
 
 	static final String SQL_ADD_BOOK_AUTHORS = "INSERT INTO `author_has_book` (`author_id`, `book_id`) VALUES ";
